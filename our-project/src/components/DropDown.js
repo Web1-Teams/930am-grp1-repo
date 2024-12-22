92047cbed29f61587081ac47543d6a8f368d01da
Lana-Khdair <s12325211@stu.najah.edu>
2024-12-23 01:10:56 +0200

    Nav

---
+++ our-project/src/components/DropDown.js
@@ -0,0 +1,23 @@
+
+import React from 'react'
+
+
+const DropDown = () => {
+
+
+
+  return (  
+    <>   
+           <a href="/home" className="block transition-color p-2 text-xl   text-blue-950  hover:text-yellow-500 hover:text-2xl">Home</a>
+           <a href="/Gaming" className="block transition-color p-2 text-xl   text-blue-950  hover:text-yellow-500 hover:text-2xl">Gaming</a>
+           <a href="/kitchen" className="block transition-color p-2 text-xl  text-blue-950  hover:text-yellow-500 hover:text-2xl">Kitchen</a>
+     </>
+    )
+
+
+
+}
+
+
+
+export default DropDown
