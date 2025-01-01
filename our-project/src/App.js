c454a00559dc6a7d14db842f7ec01ccb8c441437
RayhanHamadneh <s12323546@stu.najah.edu>
2025-01-01 21:01:01 +0200

    تعديل بسيط

--- our-project/src/App.js
+++ our-project/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import ShoppingBag from './components/ShoppingBag';
 
 function App() {
 
 
   return (
     <div className="App">
-      
+      <ShoppingBag/>
     </div>
   );
 
