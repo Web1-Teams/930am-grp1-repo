b9379acffb01bceab803697748cb96b8102c8506
AmeerIbdah <ameeribdah4@gmail.com>
2025-01-04 18:30:28 +0200

    productdetails

--- our-project/src/components/OrderSummary.js
+++ our-project/src/components/OrderSummary.js
@@ -2,11 +2,14 @@
 import React from 'react';
 
 const OrderSummary = ({ total }) => {
+  const handleBuy = () => {
+    alert('Item has been purchased!');
+  };
   return (
     <div className="order-summary">
       <p className="text-xl m-2">Order Summary:</p>
       <p className="text-lg m-1">Total: ₪{total}</p>
-      <button className="bg-blue-900 hover:bg-blue-950 text-lg mr-1 text-white font-bold py-2 px-4 rounded-lg">Checkout</button>
+      <button  onClick={handleBuy}  className="bg-blue-900 hover:bg-blue-950 text-lg mr-1 text-white font-bold py-2 px-4 rounded-lg">Checkout</button>
     </div>
   );
 }
