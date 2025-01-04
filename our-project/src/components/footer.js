8d30db9ccac6ef95bd2d3bdf029455c25d1ca43e
abdullah abuodeh <s12325494@stu.najah.edu>
2025-01-04 18:43:24 +0200

    Footer

--- our-project/src/components/footer.js
+++ our-project/src/components/footer.js
@@ -3,39 +3,47 @@ import React from "react";
 const Footerabd = () => {
   return (
     <footer className="bg-blue-200 py-16 rounded-xl shadow-lg border border-gray-300">
-      <div className="container mx-auto h-32 ">
-        <div className="flex flex-wrap px-8 ">
+      <div className="container mx-auto">
+        <div className="flex flex-wrap px-8">
+          
           <div className="w-1/4 px-4">
-            <h4 className="text-lg text-blue-950 font-medium mb-8">Company</h4>
+            <h4 className="text-lg text-blue-950 font-medium mb-8 hover:text-yellow-500 hover:text-2xl">Company</h4>
             <ul className="list-none">
               <li>
                 <a
-                  href="# "
-                  className="text-blue-950 hover:text-white transition"
+                  href="#about"
+                  className="text-blue-950 hover:text-yellow-500 "
                 >
-                  About us
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#careers"
+                  className="text-blue-950 hover:text-yellow-500 transition"
+                >
+                  Careers
                 </a>
               </li>
             </ul>
           </div>
 
+          
           <div className="w-1/4 px-4">
-            <h4 className="text-lg text-blue-950  font-medium mb-8">
-              Mobile Apps
-            </h4>
+            <h4 className="text-lg text-blue-950 font-medium mb-8 hover:text-yellow-500 hover:text-2xl">Mobile Apps</h4>
             <ul className="list-none">
               <li>
                 <a
-                  href="# "
-                  className="text-blue-950 hover:text-white transition"
+                  href="#android"
+                  className="text-blue-950 hover:text-yellow-500 transition"
                 >
                   Android
                 </a>
               </li>
               <li>
                 <a
-                  href="# "
-                  className="text-blue-950 hover:text-white transition"
+                  href="#ios"
+                  className="text-blue-950 hover:text-yellow-500 transition"
                 >
                   iOS
                 </a>
@@ -43,64 +51,64 @@ const Footerabd = () => {
             </ul>
           </div>
 
-          <div className="w-1/4 px-4">
-            <h4 className="text-lg text-blue-950 font-medium mb-8">
-              Customer Service
-            </h4>
+          
+          <div className="w-1/4 px-4 ">
+            <h4 className="text-lg text-blue-950 font-medium mb-8 hover:text-yellow-500 hover:text-2xl">Customer Service</h4>
             <ul className="list-none">
               <li>
                 <a
-                  href="# "
-                  className="text-blue-950 hover:text-white transition"
+                  href="#contact"
+                  className="text-blue-950 hover:text-yellow-500 transition"
                 >
                   Contact
                 </a>
               </li>
               <li>
                 <a
-                  href="# "
-                  className="text-blue-950 hover:text-white transition"
+                  href="#faq"
+                  className="text-blue-950 hover:text-yellow-500 transition"
                 >
-                  Questions
+                  FAQ
                 </a>
               </li>
               <li>
                 <a
-                  href="# "
-                  className="text-blue-950 hover:text-white transition"
+                  href="#return-policy"
+                  className="text-blue-950 hover:text-yellow-500 transition"
                 >
-                  Return and Exchange Policy
+                  Return & Exchange Policy
                 </a>
               </li>
             </ul>
           </div>
 
+          
           <div className="w-1/4 px-4">
             <h4 className="text-lg text-blue-950 font-medium mb-8 hover:text-yellow-500 hover:text-2xl">
-              Follow us
+              Follow Us
             </h4>
             <div className="flex space-x-4">
               <a
-                href="# "
-                className="flex items-center justify-center h-10 w-10 bg-blue-900 rounded-full text-white hover:bg-white transnitio"
+                href="#facebook"
+                className="flex items-center justify-center h-10 w-10 bg-blue-900 rounded-full text-white hover:bg-yellow-600 transition"
               >
                 <i className="fa-brands fa-facebook-f"></i>
               </a>
               <a
-                href="# "
-                className="flex items-center justify-center h-10 w-10 bg-red-200 rounded-full text-gray-500 hover:bg-white transition"
+                href="#twitter"
+                className="flex items-center justify-center h-10 w-10 bg-blue-400 rounded-full text-white hover:bg-yellow-600 transition"
               >
                 <i className="fa-brands fa-twitter"></i>
               </a>
               <a
-                href="# "
-                className="flex items-center justify-center h-10 w-10 bg-red-200 rounded-full text-gray-500 hover:bg-white transition"
+                href="#instagram"
+                className="flex items-center justify-center h-10 w-10 bg-pink-500 rounded-full text-white hover:bg-yellow-600 transition"
               >
                 <i className="fa-brands fa-instagram"></i>
               </a>
               <a
-                href="# "
-                className="flex items-center justify-center h-10 w-10 bg-red-200 rounded-full text-gray-500 hover:bg-white transition"
+                href="#linkedin"
+                className="flex items-center justify-center h-10 w-10 bg-blue-600 rounded-full text-white hover:bg-yellow-600 transition"
               >
                 <i className="fa-brands fa-linkedin-in"></i>
               </a>
@@ -108,12 +116,14 @@ const Footerabd = () => {
           </div>
         </div>
       </div>
+
+      {/* Footer Bottom */}
       <div className="mt-20 text-center">
         <p className="text-gray-500">
           <small>
             Electronice.com © 2017 - 2024. Yatsiv St 128, Jerusalem; Phone:
             123-123-123-123; All Rights Reserved. Designed by{" "}
-            <a href="Electronice.com" className="text-yellow-500 underline">
+            <a href="https://electronice.com" className="text-yellow-500 underline">
               Electronice.com
             </a>
           </small>
